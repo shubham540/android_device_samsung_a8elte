1c3658880c995996447b95a1089e82c99e4ae1ad
Andreas Schneider <asn@cryptomilk.org>
2015-01-30 16:25:56 +0100

    board: Add NFC support.

--- device_slte.mk
+++ device_slte.mk
@@ -109,6 +109,22 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/bluetooth/bcm4350_prepatch.hcd:system/vendor/firmware/bcm4350_prepatch.hcd
 
 ###########################################################
+### NFC
+###########################################################
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-sec.conf \
+	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
+
+PRODUCT_PACKAGES += \
+	com.android.nfc_extras \
+	libnfc_nci_jni \
+	libnfc-nci \
+	NfcNci \
+	Tag
+
+###########################################################
 ### AUDIO
 ###########################################################
 
